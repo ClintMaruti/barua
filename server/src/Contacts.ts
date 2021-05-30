ee1bd4e69c9755934f41ff5ea2909ab4f720fc09
ClintPy <cmaruti93@gmail.com>
2021-05-30 16:23:18 +0300

    [04. Finished Contact.tc]

---
+++ server/src/Contacts.ts
@@ -0,0 +1,65 @@
+import Nedb from "nedb";
+import * as path from "path";
+const Datastore = require("nedb");
+
+export interface IContact {
+  _id?: number;
+  name: string;
+  email: string;
+}
+
+export class Worker {
+  private db: Nedb;
+  constructor() {
+    this.db = new Datastore({
+      filename: path.join(__dirname, "contacts.db"),
+      autoload: true,
+    });
+  }
+
+  public listContacts(): Promise<IContact[]> {
+    return new Promise((inResolve, inReject) => {
+      this.db.find({}, (inError: Error, inDocs: IContact[]) => {
+        if (inError) {
+          inReject(inError);
+        } else {
+          inResolve(inDocs);
+        }
+      });
+    });
+  }
+
+  /**
+   * Add a new contact.
+   *
+   * @param  inContact The contact to add.
+   * @return           A promise that eventually resolves to an IContact object.
+   */
+  public addContact(inContact: IContact): Promise<IContact> {
+    return new Promise((inResolve, inReject) => {
+      this.db.insert(inContact, (inError: Error, inNewDoc: IContact) => {
+        if (inError) {
+          inReject(inError);
+        } else {
+          inResolve(inNewDoc);
+        }
+      });
+    });
+  }
+
+  public deleteContact(inID: string): Promise<string> {
+    return new Promise((inResolve, inReject) => {
+      this.db.remove(
+        { _id: inID },
+        {},
+        (inError: Error, inNumRemoved: Number) => {
+          if (inError) {
+            inReject(inError);
+          } else {
+            inResolve();
+          }
+        }
+      );
+    });
+  }
+}
